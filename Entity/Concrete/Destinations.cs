c465c61d00223dbcc49820c6d18a690e18fe690c
velibeyli <valibayli91@gmail.com>
2022-11-13 01:03:53 +0400

    Add project files.

---
+++ Entity/Concrete/Destinations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class Destinations
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string DayNights { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+        public bool Status { get; set; }
+    }
+}
